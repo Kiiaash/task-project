d0676ecb9b2c04b964812e57310d4e0fe61b0e77
kia sh <sheikhykia@gmail.com>
2024-12-02 12:17:33 +0330

    creating the routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('tasks',TaskController::class);
