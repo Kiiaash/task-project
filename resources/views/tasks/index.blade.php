3a1769de215ea4a76ca6892fe33f235ed99eb813
kia sh <sheikhykia@gmail.com>
2024-12-03 06:49:05 +0330

    setting up the tasks page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-task1
+<div class="card m-2 mx-4">
+    <div class="card-header text-center fw-bold fs-4">
+      Task Creator
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Add your tasks here</h5>
+      <p class="card-text">Insert your tasks, get them done later</p>
+      <form method="post" action="{{ route('tasks.store') }}">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Task:</label>
+          <textarea class="form-control form-control-lg"></textarea>
+          <div id="emailHelp" class="form-text">I'm here to help you with your tasks</div>
+        </div>
+        <div class="row">
+            <button type="submit" class="btn btn-primary bg-lg d-blcok">Add</button>
+        </div>
+      </form>
+    </div>
+  </div>
+    
+
 @endsection
