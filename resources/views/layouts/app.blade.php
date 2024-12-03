cbef224edf228d44d683170e5c193d695ac6bf09
kia sh <sheikhykia@gmail.com>
2024-12-03 09:02:25 +0330

    setting up the route for dashboard button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{ route('tasks.index') }}">Tasks</a>
+                          </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tasks',TaskController::class);
+Route::get('/dashboard',function(){
+    return view('home');
+})->name('dashboard')->middleware('auth');
