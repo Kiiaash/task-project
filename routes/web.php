1be5a70064bc9113011ad2859865bfc2e74d5c42
kia sh <sheikhykia@gmail.com>
2024-12-09 09:04:58 +0330

    modifying the routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminModifierController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
@@ -19,42 +20,42 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('tasks',TaskController::class);
-Route::resource('adminmod',AdminModifierController::class);
+Route::resource('tasks', TaskController::class);
+Route::resource('adminmod', AdminModifierController::class);
 
 
-Route::get('/managment',function(){
+Route::get('/managment', function () {
     return view('admin.main.admin_main');
 })->name('managment')->middleware('auth');
 
-Route::get('/managment_login',[ManagmentController::class, 'takelogin'])->name('login.take');
+Route::get('/managment_login', [ManagmentController::class, 'takelogin'])->name('login.take');
 
-Route::post('/check',[ManagmentController::class,'login'])->name('login.check');
+Route::post('/check', [ManagmentController::class, 'login'])->name('login.check');
 
-Route::get('/loggingout',[ManagmentController::class, 'logout'])->name('logout.dashboard');
+Route::get('/loggingout', [ManagmentController::class, 'logout'])->name('logout.dashboard');
 
-Route::get('/forgetpass',[forgetpassController::class, 'showTheForm'])->name('forget.pass');
-Route::post('/reset',[forgetpassController::class, 'reset'])->name('reset.pass');
-Route::get('/passresetform/{token}',[forgetpassController::class, 'showresetpassform'])->name('resetpass.show');
-Route::post('/passupdate',[forgetpassController::class, 'passwordUpdater'])->name('update.pass');
+Route::controller(ForgotPasswordController::class)->group(function () {
+    Route::get('/forgetpass','showTheForm')->name('forget.pass');
+    Route::post('/reset', 'reset')->name('reset.pass');
+    Route::get('/passresetform/{token}','showresetpassform')->name('resetpass.show');
+    Route::post('/passupdate','passwordUpdater')->name('update.pass');
+});
 
 
-Route::middleware(['auth'])->prefix('admin')->group(function(){
+Route::middleware(['auth'])->prefix('admin')->group(function () {
 
-    Route::get('/dashboard',function(Request $request){
+    Route::get('/dashboard', function (Request $request) {
         $path = $request->path();
-        return view('admin.sections.index',compact('path'));
+        return view('admin.sections.index', compact('path'));
     })->name('dashboard');
 
-    Route::get('/tasks',function(Request $request){
+    Route::get('/tasks', function (Request $request) {
         $path = $request->path();
-        return view('admin.sections.posts.index',compact('path'));
+        return view('admin.sections.posts.index', compact('path'));
     })->name('tasks');
 
-    Route::get('/edit',function(Request $request){
+    Route::get('/edit', function (Request $request) {
         $path = $request->path();
-        return view('admin.sections.editor.index.blade',compact('path'));
+        return view('admin.sections.editor.index.blade', compact('path'));
     })->name('edit');
-    
-
 });
